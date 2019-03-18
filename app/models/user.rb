74cf376c6dfa2d5603146cfc844fcf664f2e78b6
Your Name <you@example.com>
2019-03-18 12:40:36 +0530

    running application till chapter 12

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,9 +64,9 @@ class User < ApplicationRecord
   end
 
   # Returns true if a password reset has expired.
-  # def password_reset_expired?
-  #   reset_sent_at < 2.hours.ago
-  # end
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
   
   private
 
